e3114d18331a54a4804eed9b298bc33d603fdcaa
MohamedMahmoud01110 <mohamed.mahmoud14532@gmail.com>
2025-02-05 04:44:11 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './core/layouts/auth/auth.component';
+import { UserComponent } from './core/layouts/user/user.component';
+import { LoginComponent } from './core/auth/components/login/login.component';
+import { RegisterComponent } from './core/auth/components/register/register.component';
+import { HomeComponent } from './features/home/components/home/home.component';
+import { CategoryComponent } from './features/category/components/category/category.component';
+import { CartComponent } from './features/cart/components/cart/cart.component';
+import { NotFoundComponent } from './core/auth/components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'auth', component: AuthComponent, children: [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: '**', component: NotFoundComponent}
+  ]
+  },
+  {path:'user', component: UserComponent, children: [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path:'home', component: HomeComponent},
+    {path:'category',component:CategoryComponent},
+    {path:'cart',component:CartComponent},
+    {path: '**', component: NotFoundComponent}
+  ]},
+];
